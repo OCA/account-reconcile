64c68c7b05d5a4dc3a8262fd6c27c3faccd01fa0
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-12-18 21:22:52 +0100

    [MIGR] Module account_statement_base_completion

--- account_statement_base_completion/statement.py
+++ account_statement_base_completion/statement.py
@@ -353,8 +353,7 @@ class AccountBankSatement(Model):
             + "\n" + error_log + "-------------" + "\n"]
         log = "\n".join(log_line)
         self.write(cr, uid, [stat_id], {'completion_logs' : log}, context=context)
-        logger.notifyChannel('Bank Statement Completion', netsvc.LOG_INFO, 
-            "Bank Statement ID %s has %s lines completed"%(stat_id, number_imported))
+        self.message_post(cr, uid, [stat_id], body=_('Statement %s auto-completed for %s lines completed') % (st_number,number_imported), context=context)
         return True
     
     def button_auto_completion(self, cr, uid, ids, context=None):
