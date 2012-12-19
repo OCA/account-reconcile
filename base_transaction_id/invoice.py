598768c13fb1d42d5f33c27ad1e4f8783650f6a3
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-12-19 14:14:59 +0100

    [FIX] Import osv for osv.except

--- base_transaction_id/invoice.py
+++ base_transaction_id/invoice.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 from openerp.osv.orm import Model
-from openerp.osv import fields
+from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
 class AccountInvoice(Model):
