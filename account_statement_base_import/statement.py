e36c2db08d4f75f291df817d1fdf328b172e3b64
unknown <nicolas.bessi@camptocamp.com>
2013-01-29 09:46:57 +0100

    [IMP] historical logs

--- account_statement_base_import/statement.py
+++ account_statement_base_import/statement.py
@@ -43,6 +43,7 @@ class AccountStatementProfil(Model):
             help="Tic that box to automatically launch the completion "
                  "on each imported file using this profile."),
         'last_import_date': fields.datetime("Last Import Date"),
+        'rec_log': fields.text('log', readonly=True), # deprecated
         'import_type': fields.selection(
             get_import_type_selection,
             'Type of import',
