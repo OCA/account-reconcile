b48a46a29925624591879cd52f83f7f373427921
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2014-05-27 17:41:43 +0200

    [FIX] updated copyrights

--- account_advanced_reconcile_bank_statement/easy_reconcile.py
+++ account_advanced_reconcile_bank_statement/easy_reconcile.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Romain Deheele
-#    Copyright 2013 Camptocamp SA
+#    Author: Matthieu Dietrich
+#    Copyright 2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
