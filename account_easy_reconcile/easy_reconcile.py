2c59a3500da2ad097c9fd320154a9858d5f15f7f
Nicolas Bessi (nbessi) <nbessi@users.noreply.github.com>
2014-07-08 13:37:36 +0200

    Add missing import
    Due to merge trouble

--- account_easy_reconcile/easy_reconcile.py
+++ account_easy_reconcile/easy_reconcile.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+from datetime import datetime
 from openerp.osv import fields, osv, orm
 from openerp.tools.translate import _
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
