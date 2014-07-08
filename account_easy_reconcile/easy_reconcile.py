757b2f1ea1cf5d367aac74ebefd8b48c4c8dc134
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-07-08 11:51:38 +0200

    [MRG] [ADD] a cron to run reconciles.   By default it runs one reconcile after the other to be more atomic   and memory friendly. But you can set run_all agrument to have all reconcile   run at once

--- account_easy_reconcile/easy_reconcile.py
+++ account_easy_reconcile/easy_reconcile.py
@@ -258,7 +258,7 @@ class account_easy_reconcile(orm.Model):
                 _('Error'),
                 _('There is no history of reconciled '
                   'items on the task: %s.') % rec.name)
-     
+
     def _open_move_line_list(sefl, cr, uid, move_line_ids, name, context=None):
         return {
             'name': name,
@@ -336,3 +336,27 @@ class account_easy_reconcile(orm.Model):
         if not rec.last_history:
             self._no_history(cr, uid, rec, context=context)
         return rec.last_history.open_partial()
+
+    def run_scheduler(self, cr, uid, run_all=None, context=None):
+        """ Launch the reconcile with the oldest run
+        This function is mostly here to be used with cron task
+
+        :param run_all: if set it will ingore lookup and launch
+                    all reconciliation
+        :returns: True in case of success or raises an exception
+
+        """
+        def _get_date(reconcile):
+            return datetime.strptime(reconcile.last_history.date,
+                                     DEFAULT_SERVER_DATETIME_FORMAT)
+
+        ids = self.search(cr, uid, [], context=context)
+        assert ids, "No easy reconcile available"
+        if run_all:
+            self.run_reconcile(cr, uid, ids, context=context)
+            return True
+        reconciles = self.browse(cr, uid, ids, context=context)
+        reconciles.sort(key=_get_date)
+        older = reconciles[0]
+        self.run_reconcile(cr, uid, [older.id], context=context)
+        return True
