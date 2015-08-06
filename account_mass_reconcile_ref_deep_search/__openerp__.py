9135d4600da8bee76a1b4aab01c74c6afd0415d3
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2015-08-06 14:24:00 +0200

    Rename and migrate to 9.0 "account_advanced_reconcile_ref_deep_search"

---
+++ account_mass_reconcile_ref_deep_search/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2015-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': 'Mass Reconcile Ref Deep Search',
+    'version': '9.0.1.0.0',
+    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'category': 'Finance',
+    'website': 'http://www.camptocamp.com',
+    'depends': ['account_mass_reconcile'],
+    'data': [
+        'views/mass_reconcile_view.xml'
+    ],
+    'demo': [],
+    'test': [],
+    'auto_install': False,
+    'installable': True,
+    'images': []
+}
