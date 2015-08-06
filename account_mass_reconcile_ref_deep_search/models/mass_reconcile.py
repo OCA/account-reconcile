5d0e593eb14d99d84f2dbe4c02c7ccc4fa66659f
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2015-08-06 14:24:00 +0200

    Rename and migrate to 9.0 "account_advanced_reconcile_ref_deep_search"

---
+++ account_mass_reconcile_ref_deep_search/models/mass_reconcile.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2015-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+from openerp import api, models
+
+
+class AccountMassReconcileMethod(models.Model):
+
+    _inherit = 'account.mass.reconcile.method'
+
+    @api.model
+    def _get_all_rec_method(self):
+        _super = super(AccountMassReconcileMethod, self)
+        methods = _super.get_all_rec_method()
+        methods += [
+            ('mass.reconcile.advanced.ref.deep.search',
+             'Advanced. Partner and Ref. Deep Search'),
+        ]
+        return methods
