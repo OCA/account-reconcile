c300dd7d4068b53840ee21f7488a241aecc147d3
Damien Crier <damien.crier@camptocamp.com>
2015-09-04 09:39:07 +0200

    [FIX] replace eval() by safe_eval()

--- account_easy_reconcile/base_reconciliation.py
+++ account_easy_reconcile/base_reconciliation.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 from openerp import models, api, fields
+from openerp.tools.safe_eval import safe_eval
 from operator import itemgetter, attrgetter
 
 
@@ -111,7 +112,7 @@ class EasyReconcileBase(models.AbstractModel):
         params = []
         if self.filter:
             dummy, where, params = ml_obj._where_calc(
-                eval(self.filter)).get_sql()
+                safe_eval(self.filter)).get_sql()
             if where:
                 where = " AND %s" % where
         return where, params
