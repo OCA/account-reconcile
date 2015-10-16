14d33736f16c5f5a61c0f8bdc0c3ab182839a242
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-10-16 14:16:03 +0200

    [PORT] base_transaction_id to 9.0 - Reactivate module - move files in views and models dirs - create README.rst from description - make list of contributors - remove change for invoice created on picking as it doesn't exist anymore in Odoo Community - move overrides in bank statement as logic moved in move lines - adapt view inheritance to not depends on string attribute

--- base_transaction_id/models/sale.py
+++ base_transaction_id/models/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-#    Author: Yannick Vaucher (Camptocamp)
-#    Copyright 2012 Camptocamp SA
+#    Author: Nicolas Bessi, Yannick Vaucher
+#    Copyright 2011-2015 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -16,28 +16,22 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+from openerp import models, fields, api
 
-{'name': 'Base transaction id for financial institutes',
- 'version': '9.0.1.0.0',
- 'author': "Camptocamp,Odoo Community Association (OCA)",
- 'maintainer': 'Camptocamp',
- 'category': 'Hidden/Dependency',
- 'complexity': 'easy',
- 'depends': [
-     'account',
-     'stock_account',
-     'sale_stock',
- ],
- 'website': 'http://www.openerp.com',
- 'data': [
-     'views/invoice.xml',
-     'views/sale.xml',
-     'views/account_move_line.xml',
-     'views/base_transaction_id.xml',
- ],
- 'test': [],
- 'installable': True,
- 'images': [],
- 'auto_install': False,
- 'license': 'AGPL-3',
- }
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    transaction_id = fields.Char(
+        'Transaction ID',
+        required=False,
+        copy=False,
+        help="Transaction id from the financial institute"
+    )
+
+    @api.multi
+    def _prepare_invoice(self):
+        """ Propagate the transaction_id from the sale order to the invoice """
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['transaction_id'] = self.transaction_id
+        return invoice_vals
