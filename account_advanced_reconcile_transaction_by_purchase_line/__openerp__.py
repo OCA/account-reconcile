2e2d14aed7e9f0a77de4d924a23da9666c2b71cf
jbeficent <jordi.ballester@eficent.com>
2016-01-26 22:06:50 +0100

    [ADD] [account_advanced_reconcile_transaction_by_purchase_line]

---
+++ account_advanced_reconcile_transaction_by_purchase_line/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Romain Deheele. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Advanced Reconcile Transaction by Purchase Line',
+ 'description': """
+Advanced reconciliation method for the module account_advanced_reconcile
+========================================================================
+Reconcile rules with transaction_ref
+
+""",
+ 'version': '1.0.1',
+ 'author': "Camptocamp,Odoo Community Association (OCA)",
+ 'category': 'Finance',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['account_advanced_reconcile',
+             'account_move_line_purchase_info'],
+ 'data': ['easy_reconcile_view.xml'],
+ 'demo': [],
+ 'test': [],  # To be ported or migrate to unit tests or scenarios
+ 'auto_install': False,
+ 'installable': True,
+ 'images': []
+ }
