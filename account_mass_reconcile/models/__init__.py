4fb88fe2ffbca81f4503823bf472327d2feeab80
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-04-13 14:12:40 +0200

    Merge and migrate account_easy_reconcile and account_advanced_reconcile

---
+++ account_mass_reconcile/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import mass_reconcile
+from . import base_reconciliation
+from . import base_advanced_reconciliation
+from . import simple_reconciliation
+from . import advanced_reconciliation
+from . import mass_reconcile_history
+from . import res_config
