15604190216918744e7223a9b9e94a109baab797
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-05-24 11:51:51 +0200

    Remane import modules

---
+++ account_move_base_import/wizard/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2011 Akretion
+# © 2011-2016 Camptocamp SA
+# © 2013 Savoir-faire Linux
+# © 2014 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+from . import import_statement
