8f149a7778fb3f8e58b33c318a011c361a2cba47
Andhitia Rama <andhitia.r@gmail.com>
2016-07-01 18:45:45 +0700

    (1) Unitest fix, (2) PEP8 fix

--- account_statement_journal_visibility/tests/test_account_statement.py
+++ account_statement_journal_visibility/tests/test_account_statement.py
@@ -6,6 +6,7 @@ from openerp.tests.common import TransactionCase
 
 
 class TestAccountStatement(TransactionCase):
+
     def setUp(self, *args, **kwargs):
         result = super(TestAccountStatement, self).setUp(*args, **kwargs)
 
@@ -16,13 +17,12 @@ class TestAccountStatement(TransactionCase):
         self.view_bs = self.env.ref("account.view_bank_statement_form")
         self.view_cr = self.env.ref("account.view_bank_statement_form2")
         self.bank_journals = self.obj_journal.search(
-            [("type","=","bank")])
+            [("type", "=", "bank")])
         self.bank_journal_ids = self.bank_journals.mapped("id")
 
         self.cash_journals = self.obj_journal.search(
-            [("type","=","cash")])
+            [("type", "=", "cash")])
         self.cash_journal_ids = self.cash_journals.mapped("id")
-
 
         return result
 
@@ -30,12 +30,26 @@ class TestAccountStatement(TransactionCase):
         self.assertEqual(
             self.user_admin.bank_statement_allowed_journal_ids,
             self.bank_journals
-            )
+        )
 
-    def test_3(self):
+        self.bank_journals[0].write({
+            "bank_statement_allowed_group_ids": [(6, 0, [self.group_test.id])],
+        })
+
+        self.assertEqual(
+            len(self.bank_journals[0].bank_statement_allowed_group_ids),
+            1)
+
+    def test_2(self):
         self.assertEqual(
             self.user_admin.cash_register_allowed_journal_ids,
             self.cash_journals
-            )
-        
+        )
 
+        self.cash_journals[0].write({
+            "cash_register_allowed_group_ids": [(6, 0, [self.group_test.id])],
+        })
+
+        self.assertEqual(
+            len(self.cash_journals[0].cash_register_allowed_group_ids),
+            1)
