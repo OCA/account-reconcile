f388ac5df55563946e5aa3078574dc50e5c2cb6c
jbeficent <jordi.ballester@eficent.com>
2016-09-17 08:55:25 +0200

    [IMP] add automated tests. Remove unused files

---
+++ account_advanced_reconcile/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012-2014 Camptocamp SA - Guewen Baconnier
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_scenario_reconcile
