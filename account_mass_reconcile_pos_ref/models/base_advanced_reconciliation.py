f40b75958e035290e67b54583d0feda6c7324c83
Florent THOMAS <florent.thomas@mind-and-go.com>
2016-09-19 22:06:38 +0200

    Add implementation for matching POS moves

---
+++ account_mass_reconcile_pos_ref/models/base_advanced_reconciliation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+from openerp import models
+
+
+class MassReconcileAdvanced(models.AbstractModel):
+
+    _inherit = 'mass.reconcile.advanced'
+
+    def _base_columns(self):
+        """ Mandatory columns for move lines queries
+        An extra column aliased as ``key`` should be defined
+        in each query."""
+        aml_cols = super(MassReconcileAdvanced, self)._base_columns()
+        aml_cols.append('account_move_line.ref')
+        aml_cols.append('account_move_line.name')
+        return aml_cols
