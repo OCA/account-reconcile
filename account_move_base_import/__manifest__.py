aefcc1a0e1a1a0dae0919017d0d61a93de183a05
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-09 12:16:23 +0100

    Start work to port account_move_base_import and base_transaction_id to v10

--- account_move_base_import/__manifest__.py
+++ account_move_base_import/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2011 Akretion
+# © 2011-2016 Akretion
 # © 2011-2016 Camptocamp SA
 # © 2013 Savoir-faire Linux
 # © 2014 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': "Journal Entry base import",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': "Akretion,Camptocamp,Odoo Community Association (OCA)",
     'category': 'Finance',
     'depends': ['account'],
@@ -26,7 +26,6 @@
         'test/refund.yml',
         'test/completion_test.yml'
     ],
-    'installable': False,
-    'auto_install': False,
+    'installable': True,
     'license': 'AGPL-3',
 }
--- account_move_base_import/models/partner.py
+++ account_move_base_import/models/partner.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2011 Akretion
+# © 2011-2016 Akretion
 # © 2011-2016 Camptocamp SA
 # © 2013 Savoir-faire Linux
 # © 2014 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
