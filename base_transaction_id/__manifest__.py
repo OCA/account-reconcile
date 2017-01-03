e77aac95dc8367fbf9a94c20899345a000219b21
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-03 13:54:04 +0100

    [MIG] base_transaction_id: Migrated to 10.0

--- base_transaction_id/__manifest__.py
+++ base_transaction_id/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*
-# © 2012 Yannick Vaucher (Camptocamp)
+# © 2012 Yannick Vaucher, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Base transaction id for financial institutes',
  'version': '10.0.1.0.0',
@@ -18,4 +18,4 @@
  ],
  'installable': True,
  'license': 'AGPL-3',
-}
+ }
