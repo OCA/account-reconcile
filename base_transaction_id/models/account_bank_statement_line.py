d9f5442430145ee5a6724a53cb2afb6d38e1044c
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-03 13:54:04 +0100

    [MIG] base_transaction_id: Migrated to 10.0

--- base_transaction_id/models/account_bank_statement_line.py
+++ base_transaction_id/models/account_bank_statement_line.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016 Yannick Vaucher (Camptocamp)
+# © 2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import api, models
+from odoo import api, models
 
 
 class AccountBankStatementLine(models.Model):
