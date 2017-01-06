3a1bf1a98bdb5629f0ccb2c179fabe30905bffa5
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-01-06 13:35:40 +0100

    [MIG] account_mass_reconcile: Init mgration to 10.0

--- account_mass_reconcile/models/base_reconciliation.py
+++ account_mass_reconcile/models/base_reconciliation.py
@@ -3,8 +3,8 @@
 # © 2010 Sébastien Beau
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, fields
-from openerp.tools.safe_eval import safe_eval
+from odoo import models, api, fields
+from odoo.tools.safe_eval import safe_eval
 from operator import itemgetter
 
 
