ae830cc4e298a7729f44d6f03f436002ed019189
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-01-06 13:35:40 +0100

    [MIG] account_mass_reconcile: Init mgration to 10.0

--- account_mass_reconcile/tests/test_reconcile_history.py
+++ account_mass_reconcile/tests/test_reconcile_history.py
@@ -2,9 +2,9 @@
 # © 2014-2016 Camptocamp SA (Damien Crier)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests import common
-from openerp import fields, tools
-from openerp.modules import get_module_resource
+from odoo.tests import common
+from odoo import fields, tools
+from odoo.modules import get_module_resource
 
 
 class TestReconcileHistory(common.TransactionCase):
