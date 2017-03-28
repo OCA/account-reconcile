54ed1c86b5056d3926c44c6df22109e186fa3a64
Damien Crier <damien.crier@camptocamp.com>
2017-03-28 13:51:26 +0200

    [9.0] Add module 'account_mass_reconcile_partner_amount'

---
+++ account_mass_reconcile_partner_amount/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': 'Mass Reconcile Partner and Amount',
+    'version': '9.0.1.0.0',
+    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'category': 'Finance',
+    'website': 'http://www.camptocamp.com',
+    'depends': ['account_mass_reconcile'],
+    'data': [
+        'views/mass_reconcile_view.xml'
+    ],
+    'demo': [],
+    'test': [],
+    'auto_install': False,
+    'installable': True,
+    'images': []
+}
