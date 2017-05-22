f6fe259531d51aca30618b7225b32386c67f00d5
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-05-22 15:35:37 -0500

    [ADD] account_banking_reconciliation

---
+++ account_banking_reconciliation/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Ursa Information Systems (http://www.ursainfosystems.com>)
+# Copyright (C) 2011 NovaPoint Group LLC (<http://www.novapointgroup.com>)
+# Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
