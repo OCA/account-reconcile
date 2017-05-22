958749ee98eaa3ab000384618ddcb141d76bf4ab
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-05-22 15:35:37 -0500

    [ADD] account_banking_reconciliation

---
+++ account_banking_reconciliation/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Ursa Information Systems (http://www.ursainfosystems.com>)
+# Copyright (C) 2011 NovaPoint Group LLC (<http://www.novapointgroup.com>)
+# Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Bank Account Reconciliation",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Accounting and Financial Management",
+    "author": "NovaPoint Group LLC, Ursa Information Systems, Odoo Community Association (OCA)",
+    "website": "http://www.novapointgroup.com",
+    "depends": [
+        "account_voucher",
+    ],
+    "data": [
+        "security/account_banking_reconciliation.xml",
+        "security/ir.model.access.csv",
+        "views/account_banking_reconciliation.xml",
+        "views/account_move_line.xml",
+        "report/bank_statement_report.xml"],
+    "demo": [],
+    "installable": False,
+}
