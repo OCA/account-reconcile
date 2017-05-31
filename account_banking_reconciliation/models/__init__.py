e624c95322073610a25e3a5582a8b2733ed09d0e
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-05-31 23:16:39 +0530

    [MIG]FIX formatting

--- account_banking_reconciliation/models/__init__.py
+++ account_banking_reconciliation/models/__init__.py
@@ -4,4 +4,5 @@
 # Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_move_line, account_banking_reconciliation
+from . import account_move_line 
+from . import account_banking_reconciliation
