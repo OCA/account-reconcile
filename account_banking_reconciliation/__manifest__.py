1829fb27451950059cbde6937a0b2e8035a54dc2
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-05-31 23:16:39 +0530

    [MIG]FIX formatting

--- account_banking_reconciliation/__manifest__.py
+++ account_banking_reconciliation/__manifest__.py
@@ -8,7 +8,8 @@
     "version": "10.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting and Financial Management",
-    "author": "NovaPoint Group LLC, Ursa Information Systems, Odoo Community Association (OCA)",
+    "author": "NovaPoint Group LLC, Ursa Information Systems, "
+              "Odoo Community Association (OCA)",
     "website": "http://www.novapointgroup.com",
     "depends": [
         "account_voucher",
