3566d5c11940d84c41046f4cce98ea1fd72fd949
mreficent <miquel.raich@eficent.com>
2017-06-27 19:33:51 +0200

    [MIG] account_mass_reconcile_by_purchase_line: Migration to 9.0

--- account_mass_reconcile_by_purchase_line/models/base_reconciliation.py
+++ account_mass_reconcile_by_purchase_line/models/base_reconciliation.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# © 2015-17 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from openerp import api, models
 
 
-class EasyReconcileAdvanced(models.AbstractModel):
-
-    _inherit = 'easy.reconcile.advanced'
+class MassReconcileBase(models.AbstractModel):
+    _inherit = 'mass.reconcile.base'
 
     @api.model
     def _base_columns(self):
         """ Mandatory columns for move lines queries
         An extra column aliased as ``key`` should be defined
         in each query."""
-        aml_cols = super(EasyReconcileAdvanced, self)._base_columns()
+        aml_cols = super(MassReconcileBase, self)._base_columns()
         aml_cols.append('account_move_line.purchase_line_id')
         aml_cols.append('account_move_line.product_id')
         return aml_cols
