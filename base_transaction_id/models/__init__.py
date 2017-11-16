35b2627f6b2f9b236c3eb12f28d21f7c44e3db8e
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-16 16:46:30 +0100

    [MIG] base_transaction_id: Migration to 11.0

--- base_transaction_id/models/__init__.py
+++ base_transaction_id/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*# -*- coding: utf-8 -*-
-# © 2012-2015 Yannick Vaucher (Camptocamp)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import invoice
 from . import sale
 from . import account_move
