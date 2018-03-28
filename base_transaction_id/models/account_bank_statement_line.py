4218714c4ab96edb4ab85e31a752ee8e511bd565
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-03-28 15:16:48 +0200

    [10.0]base_transaction_id: reconciliation process should not be restricted to payable and receivable accounts

--- base_transaction_id/models/account_bank_statement_line.py
+++ base_transaction_id/models/account_bank_statement_line.py
@@ -12,13 +12,9 @@ class AccountBankStatementLine(models.Model):
     def get_reconciliation_proposition(self, excluded_ids=None):
         """ Look for transaction_ref to give them as proposition move line """
         if self.name:
-            # If the transaction has no partner, look for match in payable and
-            # receivable account anyway
-            overlook_partner = not self.partner_id
             domain = [('transaction_ref', 'ilike', self.name)]
             match_recs = self.get_move_lines_for_reconciliation(
-                excluded_ids=excluded_ids, limit=2, additional_domain=domain,
-                overlook_partner=overlook_partner)
+                excluded_ids=excluded_ids, limit=2, additional_domain=domain)
             if match_recs and len(match_recs) == 1:
                 return match_recs
         _super = super(AccountBankStatementLine, self)
