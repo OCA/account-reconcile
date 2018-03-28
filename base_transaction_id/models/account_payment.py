bd43501e45ede857acca032d443fb7ea36c008cb
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-03-28 15:11:22 +0200

    [FIX] if a transaction_id is set on an invoice, propagate it to the payment move line

--- base_transaction_id/models/account_payment.py
+++ base_transaction_id/models/account_payment.py
@@ -16,6 +16,8 @@ class AccountPayment(models.Model):
         help="Transaction id from the financial institute"
     )
 
+    invoice_ids = fields.Many2many(inverse='_inverse_invoice_ids')
+
     @api.multi
     def _get_liquidity_move_line_vals(self, amount):
         self.ensure_one()
@@ -23,4 +25,13 @@ class AccountPayment(models.Model):
             amount)
         if self.transaction_id:
             vals['name'] = self.transaction_id
+            vals['transaction_ref'] = self.transaction_id
         return vals
+
+    @api.multi
+    def _inverse_invoice_ids(self):
+        for rec in self:
+            invoices = rec.invoice_ids.filtered(lambda x: x.transaction_id)
+            if not rec.transaction_id and invoices:
+                rec.transaction_id = ", ".join(invoices.mapped(
+                    'transaction_id'))
