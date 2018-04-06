bfbe6ba62173c7e9f93ae083a5bb4dfef0084c3d
Damien Crier <damien.crier@camptocamp.com>
2018-04-06 15:10:20 +0200

    [MIG] account_mass_reconcile_transaction_ref: Migration to 10.0

--- account_mass_reconcile_transaction_ref/__manifest__.py
+++ account_mass_reconcile_transaction_ref/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2013-2016 Camptocamp SA
+# © 2013-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Mass Reconcile Transaction Ref',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': "Camptocamp,Odoo Community Association (OCA)",
     'category': 'Finance',
     'website': 'http://www.camptocamp.com',
@@ -18,6 +18,6 @@
     'demo': [],
     'test': [],
     'auto_install': False,
-    'installable': False,
+    'installable': True,
     'images': []
 }
