dd54e784292bd9f11938a5d45335929f70d373c7
Jaume Planas <jaume.planas@minorisa.net>
2018-04-06 16:22:47 +0200

    [MIG] account_partner_reconcile: Migration to 11.0

--- account_partner_reconcile/__manifest__.py
+++ account_partner_reconcile/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 #        (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Account Partner Reconcile",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'author': 'Eficent,'
               'Odoo Community Association (OCA), ',
