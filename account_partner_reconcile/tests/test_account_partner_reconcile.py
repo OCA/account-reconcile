57839ed4582e0cf8048a64a1e72b7cd7419547da
Jaume Planas <jaume.planas@minorisa.net>
2018-04-06 16:22:47 +0200

    [MIG] account_partner_reconcile: Migration to 11.0

--- account_partner_reconcile/tests/test_account_partner_reconcile.py
+++ account_partner_reconcile/tests/test_account_partner_reconcile.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -10,29 +9,30 @@ class TestAccountPartnerReconcile(TransactionCase):
     """
         Tests for Account Partner Reconcile.
     """
+
     def setUp(self):
         super(TestAccountPartnerReconcile, self).setUp()
 
         self.partner1 = self.env.ref('base.res_partner_1')
 
     def test_account_partner_reconcile(self):
-
         res = self.partner1.action_open_reconcile()
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.client',
-                'tag': 'manual_reconciliation_view',
-            },
-            res,
+
+        # assertDictContainsSubset is deprecated in Python <3.2
+        expect = {
+            'type': 'ir.actions.client',
+            'tag': 'manual_reconciliation_view',
+        }
+        self.assertDictEqual(
+            expect, {k: v for k, v in res.items() if k in expect},
             'There was an error and the manual_reconciliation_view '
-            'couldn\'t be opened.'
-        )
-        self.assertDictContainsSubset(
-            {
-                'partner_ids': self.partner1.ids,
-                'show_mode_selector': True,
-            },
-            res['context'],
+            'couldn\'t be opened.')
+
+        expect = {
+            'partner_ids': self.partner1.ids,
+            'show_mode_selector': True,
+        }
+        self.assertDictEqual(
+            expect, {k: v for k, v in res['context'].items() if k in expect},
             'There was an error and the manual_reconciliation_view '
-            'couldn\'t be opened.'
-        )
+            'couldn\'t be opened.')
