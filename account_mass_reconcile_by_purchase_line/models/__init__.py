f57f4c519a9bbb3d380ee086c9b2ec5a098bbeb2
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-07-12 20:28:05 +0200

    [MIG] account_mass_reconcile_by_purchase_line: Migration to 11.0

--- account_mass_reconcile_by_purchase_line/models/__init__.py
+++ account_mass_reconcile_by_purchase_line/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import mass_reconcile
 from . import base_advanced_reconciliation
+from . import simple_reconciliation
