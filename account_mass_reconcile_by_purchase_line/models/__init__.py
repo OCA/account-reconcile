af6a5119c3332b5dee79e5c79ebb524c54f7cc7f
mreficent <miquel.raich@eficent.com>
2018-07-13 12:37:18 +0200

    [FIX] Finish the migration to v11

--- account_mass_reconcile_by_purchase_line/models/__init__.py
+++ account_mass_reconcile_by_purchase_line/models/__init__.py
@@ -1,3 +1,3 @@
 from . import mass_reconcile
 from . import base_advanced_reconciliation
-from . import simple_reconciliation
+from . import advanced_reconciliation
--- account_mass_reconcile_by_purchase_line/models/mass_reconcile.py
+++ account_mass_reconcile_by_purchase_line/models/mass_reconcile.py
@@ -1,16 +1,17 @@
 # © 2015-18 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountMassReconcileMethod(models.Model):
     _inherit = 'account.mass.reconcile.method'
 
-    @api.model
-    def _get_reconcilation_methods(self):
-        methods = super(AccountMassReconcileMethod,
-                        self)._get_reconcilation_methods()
+    @staticmethod
+    def _get_reconcilation_methods():
+        methods = super(
+            AccountMassReconcileMethod, AccountMassReconcileMethod).\
+            _get_reconcilation_methods()
         methods += [
             ('mass.reconcile.advanced.by.purchase.line',
              'Advanced. Product, purchase order line.'),
