c45c422f81fd4b2fd724ab95f458dbae0a008f01
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-10-04 17:21:28 +0200

    Update manifest and split readme

--- account_mass_reconcile_transaction_ref/__manifest__.py
+++ account_mass_reconcile_transaction_ref/__manifest__.py
@@ -6,7 +6,7 @@
     'version': '10.0.1.0.0',
     'author': "Camptocamp,Odoo Community Association (OCA)",
     'category': 'Finance',
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/OCA/account-reconcile',
     'license': 'AGPL-3',
     'depends': [
         'account_mass_reconcile',
@@ -15,9 +15,6 @@
     'data': [
         'views/mass_reconcile_view.xml'
     ],
-    'demo': [],
-    'test': [],
     'auto_install': False,
     'installable': True,
-    'images': []
 }
---
+++ account_mass_reconcile_transaction_ref/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Romain Deheele <romain.deheele@camptocamp.com>
+* Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
