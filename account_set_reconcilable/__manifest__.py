c9df09f407a87557ef4960f2a224d2fc1fc5bb0a
mreficent <miquel.raich@eficent.com>
2018-10-30 12:16:45 +0100

    [MIG] account_set_reconcilable: Backport to 10.0

--- account_set_reconcilable/__manifest__.py
+++ account_set_reconcilable/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -5,7 +6,7 @@
     "name": "Account Set Reconcilable",
     "summary": "Allows to set as reconcilable a non reconcilable"
                "account that already have journal items.",
-    "version": "11.0.1.0.0",
+    "version": "10.0.1.0.0",
     "depends": ["account"],
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-reconcile",
--- account_set_reconcilable/models/account_account.py
+++ account_set_reconcilable/models/account_account.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
