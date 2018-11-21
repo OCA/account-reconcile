70fca4e2d29def1a60a2a3e474708ab367c40bcf
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-11-21 18:46:02 +0100

    Finish migration to v10.0

--- account_mass_reconcile_ref_deep_search/__init__.py
+++ account_mass_reconcile_ref_deep_search/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015-2016 Camptocamp SA
+# Copyright 2015-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from . import models
--- account_mass_reconcile_ref_deep_search/models/__init__.py
+++ account_mass_reconcile_ref_deep_search/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015-2018 Camptocamp SA
+# Copyright 2015-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from . import mass_reconcile
 from . import advanced_reconciliation
