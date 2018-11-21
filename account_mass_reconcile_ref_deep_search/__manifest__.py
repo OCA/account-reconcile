f802a891af2b403c2b08430b53c1226825c6e6e7
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-11-21 18:46:02 +0100

    Finish migration to v10.0

--- account_mass_reconcile_ref_deep_search/__manifest__.py
+++ account_mass_reconcile_ref_deep_search/__manifest__.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2015-2018 Camptocamp SA
+# Copyright 2015-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Mass Reconcile Ref Deep Search',
     'version': '10.0.1.0.0',
-    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'author': "Camptocamp, Odoo Community Association (OCA)",
     'category': 'Finance',
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/account-reconcile',
     'license': 'AGPL-3',
     'depends': ['account_mass_reconcile'],
     'data': [
         'views/mass_reconcile_view.xml'
     ],
-    'demo': [],
-    'test': [],
-    'auto_install': False,
     'installable': True,
-    'images': []
 }
