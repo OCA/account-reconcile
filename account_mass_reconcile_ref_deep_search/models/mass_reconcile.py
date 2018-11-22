fb7632cb324b305be86d5ef5fd24cc8a6601aa3c
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-11-22 12:07:22 +0100

    Finish migration to v11.0

--- account_mass_reconcile_ref_deep_search/models/mass_reconcile.py
+++ account_mass_reconcile_ref_deep_search/models/mass_reconcile.py
@@ -1,16 +1,14 @@
 # Copyright 2015-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-from odoo import api, models
+from odoo import models
 
 
 class AccountMassReconcileMethod(models.Model):
 
     _inherit = 'account.mass.reconcile.method'
 
-    @api.model
-    def _get_all_rec_method(self):
-        _super = super(AccountMassReconcileMethod, self)
-        methods = _super._get_all_rec_method()
+    def _selection_name(self):
+        methods = super()._selection_name()
         methods += [
             ('mass.reconcile.advanced.ref.deep.search',
              'Advanced. Partner and Ref. Deep Search'),
