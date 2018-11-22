fe68e180d1169ae05ea0af0c5aca31ff32db8946
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-11-22 13:13:36 +0100

    fixup! Finish migration to v11.0

--- account_mass_reconcile_ref_deep_search/models/mass_reconcile.py
+++ account_mass_reconcile_ref_deep_search/models/mass_reconcile.py
@@ -1,6 +1,6 @@
 # Copyright 2015-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-from odoo import api, models
+from odoo import models
 
 
 class AccountMassReconcileMethod(models.Model):
