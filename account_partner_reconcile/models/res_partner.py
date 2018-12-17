343d847b41a1af86b01b596f5f3b6016cee2a485
Beñat Jimenez <benat.jimenez@eficent.com>
2018-12-17 10:10:50 +0100

    [MIG] account_partner_reconcile: Migration to 12.0

--- account_partner_reconcile/models/res_partner.py
+++ account_partner_reconcile/models/res_partner.py
@@ -1,4 +1,4 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-19 Eficent Business and IT Consulting Services S.L.
 #        (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
---
+++ account_partner_reconcile/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module adds a button "Match Payments" in the customer & suppliers form
+view to allow to start the matching of invoices & payments for that partner.
