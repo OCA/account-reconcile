352b2d24ff75425925b5a25e2182ed9190a748e1
mpanarin <m.panarin@mobilunity.com>
2019-04-02 14:16:03 +0300

    [12.0][FIX] base_transaction_id template extension not working
    and breaking the whole view

--- base_transaction_id/__manifest__.py
+++ base_transaction_id/__manifest__.py
@@ -1,9 +1,8 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 {
     "name": "Base transaction ID for financial institutes",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Hidden/Dependency",
@@ -14,10 +13,6 @@
     "data": [
         "views/invoice.xml",
         "views/sale.xml",
-        "views/account_move_line.xml",
-    ],
-    "qweb": [
-        "static/src/xml/account_reconciliation.xml",
     ],
     "installable": True,
     "license": "AGPL-3",
