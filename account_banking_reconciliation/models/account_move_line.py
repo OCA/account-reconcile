6b589d9bf79bf3eb56718f05c7ebfc98c29c5dd5
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-05-25 15:51:10 +0530

    [WIP][MIG][11.0] account_banking_reconciliation

--- account_banking_reconciliation/models/account_move_line.py
+++ account_banking_reconciliation/models/account_move_line.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2015 Ursa Information Systems (http://www.ursainfosystems.com>)
+# Copyright (C) 2019 Open Source Integrators
+# <https://www.opensourceintegrators.com>
 # Copyright (C) 2011 NovaPoint Group LLC (<http://www.novapointgroup.com>)
 # Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
