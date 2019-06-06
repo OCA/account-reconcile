f433f3aa147ae4a974c1738734816fea96468c67
mreficent <miquel.raich@eficent.com>
2019-06-06 14:18:53 +0200

    [MIG] account_mass_reconcile_by_purchase_line: Migration to 12.0

--- account_mass_reconcile_by_purchase_line/__manifest__.py
+++ account_mass_reconcile_by_purchase_line/__manifest__.py
@@ -4,13 +4,13 @@
 {
     "name": "Account Mass Reconcile by Purchase Line",
     "summary": "Allows to reconcile based on the PO line",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/bank-statement-reconcile",
+    "website": "https://github.com/OCA/account-reconcile",
     "category": "Finance",
     "depends": ["account_mass_reconcile",
-                "account_move_line_purchase_info"
+                "account_move_line_purchase_info",
                 ],
     "license": "AGPL-3",
     "data": [
