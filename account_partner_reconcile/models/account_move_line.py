14bfdef792f9b77b058cca51ef406fb6bf190ddb
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-07-01 18:03:32 +0300

    [ADD] create addon: account_reconcile_restrict_partner_mismatch

---
+++ account_partner_reconcile/models/account_move_line.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.multi
+    def reconcile(self, writeoff_acc_id=False, writeoff_journal_id=False):
+        # to be consistent with parent method
+        if not self:
+            return True
+        partners = set()
+        for line in self:
+            if (line.account_id.internal_type in ('receivable', 'payable')):
+                partners.add(line.partner_id.id)
+        if len(partners) > 1:
+            raise UserError(_('The partner has to be the same on all lines for receivable and payable accounts!'))
+        if len(partners) and not all([l.partner_id for l in self]):
+            raise UserError(_('You cannot match entries with and without partner!'))
+        return super(AccountMoveLine, self).reconcile(writeoff_acc_id, writeoff_journal_id)
---
+++ account_partner_reconcile/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Prohibits items reconcilation for different partners, and items with partner and
+without it.
