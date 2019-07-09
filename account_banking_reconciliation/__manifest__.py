890efbaa30b6a8a3a85dc5c4f0f4d8d8c6990981
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-07-09 19:33:09 +0530

    [12.0][MIG] account_banking_reconciliation

--- account_banking_reconciliation/__manifest__.py
+++ account_banking_reconciliation/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Bank Account Reconciliation",
     "summary": "Check transactions that cleared the bank",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting and Financial Management",
     "author": "NovaPoint Group LLC, "
@@ -14,7 +14,6 @@
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-reconcile",
     "depends": [
-        "account_invoicing",
         "account_voucher",
     ],
     "data": [
