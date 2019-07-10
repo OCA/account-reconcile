59c9d99da0b1019986a5832e0055ac9630a0783e
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-07-10 13:40:48 +0200

    fix changed line

--- auto_reconcile_structured_ref/models/account_bank_statement_line.py
+++ auto_reconcile_structured_ref/models/account_bank_statement_line.py
@@ -67,7 +67,8 @@ class AccountBankStatementLine(models.Model):
             if aml.account_id.internal_type == 'liquidity':
                 payment_aml_rec = (payment_aml_rec | aml)
             else:
-                amount = self.amount
+                amount = aml.currency_id and aml.amount_residual_currency \
+                    or aml.amount_residual
                 counterpart_aml_dicts.append({
                     'name': aml.name if aml.name != '/' else aml.move_id.name,
                     'debit': amount < 0 and -amount or 0,
