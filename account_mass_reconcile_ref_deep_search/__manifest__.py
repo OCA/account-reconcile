6a01db446b5e2af1c58393f10c9c18c2185a2e15
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-07-26 12:01:41 +0300

    [MIG][12.0] account_mass_reconcile_ref_deep_search: migration
    [UPD] Update account_mass_reconcile_ref_deep_search.pot

--- account_mass_reconcile_ref_deep_search/__manifest__.py
+++ account_mass_reconcile_ref_deep_search/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2015-2018 Camptocamp SA
+# Copyright 2015-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Mass Reconcile Ref Deep Search',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "Camptocamp, Odoo Community Association (OCA)",
     'category': 'Finance',
     'website': 'https://github.com/account-reconcile',
