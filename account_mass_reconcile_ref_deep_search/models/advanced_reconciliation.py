f229cf822cd22ca504b60115c3467dcea23a6e3a
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-07-26 12:01:41 +0300

    [MIG][12.0] account_mass_reconcile_ref_deep_search: migration

--- account_mass_reconcile_ref_deep_search/models/advanced_reconciliation.py
+++ account_mass_reconcile_ref_deep_search/models/advanced_reconciliation.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2018 Camptocamp SA
+# Copyright 2015-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from odoo import _, models
 from itertools import product
