c26dcc5575c5654165a119f5e03821ee5092a050
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-09-26 09:03:35 +0200

    [10.0][MIG] account_skip_bank_reconciliation

--- account_skip_bank_reconciliation/__manifest__.py
+++ account_skip_bank_reconciliation/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -5,10 +6,10 @@
     "name": "Account Skip Bank Reconciliation",
     "summary": "Allows to exclude from bank statement reconciliation "
                "all journal items of a reconcilable account",
-    "version": "11.0.1.0.0",
+    "version": "10.0.1.0.0",
     "depends": ["account"],
     "author": "Eficent, Odoo Community Association (OCA)",
-    "website": "http://www.github.com/OCA/account-reconcile",
+    "website": "https://www.github.com/OCA/account-reconcile",
     "category": "Finance",
     "data": [
         "views/account_view.xml",
