189d292197bacebf6ab62744de78b80f4508ef55
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-10-09 15:58:19 +0200

    fixup! [12.0] base_transaction_id: Fix migration

--- base_transaction_id/__manifest__.py
+++ base_transaction_id/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Base transaction ID for financial institutes",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Hidden/Dependency",
