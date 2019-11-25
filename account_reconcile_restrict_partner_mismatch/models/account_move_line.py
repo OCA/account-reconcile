00c2e523c4cc05cd7e0f81850a4af0ee2bf046a7
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-25 15:07:08 -0500

    [IMP] account_reconcile_restrict_partner_mismatch: black, isort

--- account_reconcile_restrict_partner_mismatch/models/account_move_line.py
+++ account_reconcile_restrict_partner_mismatch/models/account_move_line.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
 from odoo.tools import config
 
@@ -11,8 +11,7 @@ class AccountMoveLine(models.Model):
 
     @api.multi
     def reconcile(self, writeoff_acc_id=False, writeoff_journal_id=False):
-        if (config['test_enable']
-                and not self.env.context.get('test_partner_mismatch')):
+        if config["test_enable"] and not self.env.context.get("test_partner_mismatch"):
             return super().reconcile(writeoff_acc_id, writeoff_journal_id)
 
         # to be consistent with parent method
@@ -20,10 +19,13 @@ class AccountMoveLine(models.Model):
             return True
         partners = set()
         for line in self:
-            if line.account_id.internal_type in ('receivable', 'payable'):
+            if line.account_id.internal_type in ("receivable", "payable"):
                 partners.add(line.partner_id.id)
         if len(partners) > 1:
-            raise UserError(_('The partner has to be the same on all'
-                              ' lines for receivable and payable accounts!'))
-        return super().reconcile(
-            writeoff_acc_id, writeoff_journal_id)
+            raise UserError(
+                _(
+                    "The partner has to be the same on all"
+                    " lines for receivable and payable accounts!"
+                )
+            )
+        return super().reconcile(writeoff_acc_id, writeoff_journal_id)
