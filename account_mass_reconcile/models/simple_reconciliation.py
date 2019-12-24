dee51857deede2961d1a046d95dac6cda7578d50
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 14:51:16 +0100

    [MIG] account_mass_reconcile: Migration to 13.0

--- account_mass_reconcile/models/simple_reconciliation.py
+++ account_mass_reconcile/models/simple_reconciliation.py
@@ -2,18 +2,18 @@
 # Copyright 2010 Sébastien Beau
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class MassReconcileSimple(models.AbstractModel):
     _name = "mass.reconcile.simple"
     _inherit = "mass.reconcile.base"
+    _description = "Mass Reconcile Simple"
 
     # has to be subclassed
     # field name used as key for matching the move lines
     _key_field = None
 
-    @api.multi
     def rec_auto_lines_simple(self, lines):
         if self._key_field is None:
             raise ValueError("_key_field has to be defined")
@@ -67,6 +67,7 @@ class MassReconcileSimple(models.AbstractModel):
 class MassReconcileSimpleName(models.TransientModel):
     _name = "mass.reconcile.simple.name"
     _inherit = "mass.reconcile.simple"
+    _description = "Mass Reconcile Simple Name"
 
     # has to be subclassed
     # field name used as key for matching the move lines
@@ -76,6 +77,7 @@ class MassReconcileSimpleName(models.TransientModel):
 class MassReconcileSimplePartner(models.TransientModel):
     _name = "mass.reconcile.simple.partner"
     _inherit = "mass.reconcile.simple"
+    _description = "Mass Reconcile Simple Partner"
 
     # has to be subclassed
     # field name used as key for matching the move lines
@@ -85,6 +87,7 @@ class MassReconcileSimplePartner(models.TransientModel):
 class MassReconcileSimpleReference(models.TransientModel):
     _name = "mass.reconcile.simple.reference"
     _inherit = "mass.reconcile.simple"
+    _description = "Mass Reconcile Simple Reference"
 
     # has to be subclassed
     # field name used as key for matching the move lines
