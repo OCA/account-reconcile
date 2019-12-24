049153ed76e501df28a64a09c5360baaf0bc7548
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 13:11:07 +0100

    [IMP] account_mass_reconcile: black, isort

--- account_mass_reconcile/models/simple_reconciliation.py
+++ account_mass_reconcile/models/simple_reconciliation.py
@@ -2,12 +2,12 @@
 # Copyright 2010 Sébastien Beau
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class MassReconcileSimple(models.AbstractModel):
-    _name = 'mass.reconcile.simple'
-    _inherit = 'mass.reconcile.base'
+    _name = "mass.reconcile.simple"
+    _inherit = "mass.reconcile.base"
 
     # has to be subclassed
     # field name used as key for matching the move lines
@@ -19,27 +19,26 @@ class MassReconcileSimple(models.AbstractModel):
             raise ValueError("_key_field has to be defined")
         count = 0
         res = []
-        while (count < len(lines)):
+        while count < len(lines):
             for i in range(count + 1, len(lines)):
                 if lines[count][self._key_field] != lines[i][self._key_field]:
                     break
                 check = False
-                if lines[count]['credit'] > 0 and lines[i]['debit'] > 0:
+                if lines[count]["credit"] > 0 and lines[i]["debit"] > 0:
                     credit_line = lines[count]
                     debit_line = lines[i]
                     check = True
-                elif lines[i]['credit'] > 0 and lines[count]['debit'] > 0:
+                elif lines[i]["credit"] > 0 and lines[count]["debit"] > 0:
                     credit_line = lines[i]
                     debit_line = lines[count]
                     check = True
                 if not check:
                     continue
                 reconciled, dummy = self._reconcile_lines(
-                    [credit_line, debit_line],
-                    allow_partial=False
-                    )
+                    [credit_line, debit_line], allow_partial=False
+                )
                 if reconciled:
-                    res += [credit_line['id'], debit_line['id']]
+                    res += [credit_line["id"], debit_line["id"]]
                     del lines[i]
                     break
             count += 1
@@ -56,11 +55,9 @@ class MassReconcileSimple(models.AbstractModel):
         where += " AND account_move_line.%s IS NOT NULL " % self._key_field
 
         where2, params2 = self._get_filter()
-        query = ' '.join((
-            select,
-            self._from_query(),
-            where, where2,
-            self._simple_order()))
+        query = " ".join(
+            (select, self._from_query(), where, where2, self._simple_order())
+        )
 
         self.env.cr.execute(query, params + params2)
         lines = self.env.cr.dictfetchall()
@@ -68,27 +65,27 @@ class MassReconcileSimple(models.AbstractModel):
 
 
 class MassReconcileSimpleName(models.TransientModel):
-    _name = 'mass.reconcile.simple.name'
-    _inherit = 'mass.reconcile.simple'
+    _name = "mass.reconcile.simple.name"
+    _inherit = "mass.reconcile.simple"
 
     # has to be subclassed
     # field name used as key for matching the move lines
-    _key_field = 'name'
+    _key_field = "name"
 
 
 class MassReconcileSimplePartner(models.TransientModel):
-    _name = 'mass.reconcile.simple.partner'
-    _inherit = 'mass.reconcile.simple'
+    _name = "mass.reconcile.simple.partner"
+    _inherit = "mass.reconcile.simple"
 
     # has to be subclassed
     # field name used as key for matching the move lines
-    _key_field = 'partner_id'
+    _key_field = "partner_id"
 
 
 class MassReconcileSimpleReference(models.TransientModel):
-    _name = 'mass.reconcile.simple.reference'
-    _inherit = 'mass.reconcile.simple'
+    _name = "mass.reconcile.simple.reference"
+    _inherit = "mass.reconcile.simple"
 
     # has to be subclassed
     # field name used as key for matching the move lines
-    _key_field = 'ref'
+    _key_field = "ref"
