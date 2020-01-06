99995dfd24a5fb2b968fc3ea6487862652878b02
kittiu <kittiu@gmail.com>
2020-01-06 20:22:00 +0700

    [13.0][IMP] account_move_reconcile_helper: black, isort

--- account_move_reconcile_helper/__manifest__.py
+++ account_move_reconcile_helper/__manifest__.py
@@ -2,16 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Move Reconcile Helper',
-    'summary': "Provides tools to facilitate reconciliation",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-reconcile',
-    'depends': [
-        'account_balance_line',
-    ],
-    'data': [
-        'views/account_move_line.xml',
-    ],
+    "name": "Account Move Reconcile Helper",
+    "summary": "Provides tools to facilitate reconciliation",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-reconcile",
+    "depends": ["account_balance_line"],
+    "data": ["views/account_move_line.xml"],
 }
--- account_move_reconcile_helper/models/account_move_line.py
+++ account_move_reconcile_helper/models/account_move_line.py
@@ -6,21 +6,24 @@ from odoo import api, fields, models
 
 class AccountMoveLine(models.Model):
 
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     partial_reconciliation_in_progress = fields.Boolean(
-        compute='_compute_partial_reconciliation_in_progress')
+        compute="_compute_partial_reconciliation_in_progress"
+    )
     reconcile_line_ids = fields.One2many(
-        compute='_compute_reconciled_lines',
-        comodel_name='account.move.line',
-        string="Reconciled lines")
+        compute="_compute_reconciled_lines",
+        comodel_name="account.move.line",
+        string="Reconciled lines",
+    )
 
     @api.multi
-    @api.depends('matched_debit_ids', 'matched_credit_ids')
+    @api.depends("matched_debit_ids", "matched_credit_ids")
     def _compute_partial_reconciliation_in_progress(self):
         for rec in self:
-            rec.partial_reconciliation_in_progress = (
-                bool(rec.matched_debit_ids) or bool(rec.matched_credit_ids))
+            rec.partial_reconciliation_in_progress = bool(
+                rec.matched_debit_ids
+            ) or bool(rec.matched_credit_ids)
 
     @api.multi
     def _compute_reconciled_lines(self):
@@ -45,10 +48,9 @@ class AccountMoveLine(models.Model):
             if line.full_reconcile_id:
                 matched_lines = line.full_reconcile_id.reconciled_line_ids
             elif line.credit > 0:
-                matched_lines = line.matched_debit_ids.mapped('debit_move_id')
+                matched_lines = line.matched_debit_ids.mapped("debit_move_id")
             else:
-                matched_lines = line.matched_credit_ids.mapped(
-                    'credit_move_id')
+                matched_lines = line.matched_credit_ids.mapped("credit_move_id")
 
             if not matched_lines:
                 continue
@@ -58,14 +60,12 @@ class AccountMoveLine(models.Model):
             for matched_line in matched_lines:
                 if matched_line not in move_lines:
                     move_lines |= matched_line
-                    move_lines |= matched_line._get_reconciled_lines(
-                        move_lines)
+                    move_lines |= matched_line._get_reconciled_lines(move_lines)
 
         return move_lines
 
     @api.multi
     def open_full_reconcile_view(self):
-        action = self.env.ref('account.action_account_moves_all_a').read()[0]
-        action['domain'] = [
-            ('id', 'in', self.mapped('reconcile_line_ids').ids)]
+        action = self.env.ref("account.action_account_moves_all_a").read()[0]
+        action["domain"] = [("id", "in", self.mapped("reconcile_line_ids").ids)]
         return action
