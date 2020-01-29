9278512cbc933b23ca2a1028a6e6a929eeedfb08
mreficent <miquel.raich@eficent.com>
2020-01-29 13:04:41 +0100

    [IMP] account_partner_reconcile: black, isort, prettier

--- account_partner_reconcile/models/res_partner.py
+++ account_partner_reconcile/models/res_partner.py
@@ -6,22 +6,24 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.multi
     def action_open_reconcile(self):
         # Open reconciliation view for customers and suppliers
-        reconcile_mode = self.env.context.get('reconcile_mode', False)
+        reconcile_mode = self.env.context.get("reconcile_mode", False)
         accounts = self.property_account_payable_id
-        if reconcile_mode == 'customers':
+        if reconcile_mode == "customers":
             accounts = self.property_account_receivable_id
 
-        action_context = {'show_mode_selector': True,
-                          'partner_ids': [self.id, ],
-                          'mode': reconcile_mode,
-                          'account_ids': accounts.ids}
+        action_context = {
+            "show_mode_selector": True,
+            "partner_ids": [self.id],
+            "mode": reconcile_mode,
+            "account_ids": accounts.ids,
+        }
         return {
-            'type': 'ir.actions.client',
-            'tag': 'manual_reconciliation_view',
-            'context': action_context,
+            "type": "ir.actions.client",
+            "tag": "manual_reconciliation_view",
+            "context": action_context,
         }
