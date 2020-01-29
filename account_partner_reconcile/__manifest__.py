fa7ca6c7bef28028f91b0ba6fb8b31ec114bf625
mreficent <miquel.raich@eficent.com>
2020-01-29 13:04:41 +0100

    [IMP] account_partner_reconcile: black, isort, prettier

--- account_partner_reconcile/__manifest__.py
+++ account_partner_reconcile/__manifest__.py
@@ -3,18 +3,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Account Partner Reconcile",
-    'version': '12.0.1.0.1',
-    'category': 'Accounting',
-    'author': 'Eficent,'
-              'Odoo Community Association (OCA), ',
-    'website': 'https://github.com/OCA/account-reconcile',
-    'license': 'AGPL-3',
-    "depends": [
-        'account',
-    ],
-    "data": [
-        'views/res_partner_view.xml',
-    ],
-    "installable": True
+    "name": "Account Partner Reconcile",
+    "version": "12.0.1.0.1",
+    "category": "Accounting",
+    "author": "Eficent," "Odoo Community Association (OCA), ",
+    "website": "https://github.com/OCA/account-reconcile",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
 }
