76444be005b6a95021c153101ee55ea4edc21f27
Enric Tobella <etobella@creublanca.es>
2020-03-09 12:54:45 +0100

    [MIG][12.0] account_reconciliation_widget_partial

--- account_reconciliation_widget_partial/__manifest__.py
+++ account_reconciliation_widget_partial/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Account Reconciliation Widget Partial',
     'summary': """
         Allow to modifiy the reconcile amount for partial payments""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-reconcile',
