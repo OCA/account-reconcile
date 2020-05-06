78d00bec3b8b73ecc1c3ec3ad378d54d29a809ae
Pierrick Brun <pierrick.brun@akretion.com>
2020-05-06 13:39:31 +0200

    [IMP] add completion rule matching Stripe usage

--- account_move_stripe_import/__manifest__.py
+++ account_move_stripe_import/__manifest__.py
@@ -5,16 +5,20 @@
 {
     "name": "Account Move Stripe Import",
     "summary": "Import Deposit/Payout details from Stripe",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Finance",
-    "website": "www.akretion.com",
+    "website": "https://github.com/OCA/account-reconcile",
     "author": "Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "external_dependencies": {"python": [], "bin": [],},
     "depends": ["base_transaction_id", "account_move_base_import"],
-    "data": ["wizards/import_statement_view.xml", "data/cron.xml",],
+    "data": [
+        "wizards/import_statement_view.xml",
+        "data/cron.xml",
+        "data/completion_rule_data.xml",
+    ],
     "demo": [],
     "qweb": [],
 }
--- account_move_stripe_import/models/__init__.py
+++ account_move_stripe_import/models/__init__.py
@@ -1,1 +1,2 @@
 from . import account_journal
+from . import account_move
