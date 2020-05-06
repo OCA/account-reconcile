6c70afe237209e1a4f263a2cc9875836eeb1c469
Pierrick Brun <pierrick.brun@akretion.com>
2020-05-06 09:06:08 +0200

    black and remove encoding pragma

--- account_move_stripe_import/__manifest__.py
+++ account_move_stripe_import/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -9,24 +8,13 @@
     "version": "10.0.1.0.0",
     "category": "Finance",
     "website": "www.akretion.com",
-    'author': "Akretion,Odoo Community Association (OCA)",
+    "author": "Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    "depends": [
-        "base_transaction_id",
-        "account_move_base_import",
-    ],
-    "data": [
-        "wizards/import_statement_view.xml",
-        "data/cron.xml",
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
+    "external_dependencies": {"python": [], "bin": [],},
+    "depends": ["base_transaction_id", "account_move_base_import"],
+    "data": ["wizards/import_statement_view.xml", "data/cron.xml",],
+    "demo": [],
+    "qweb": [],
 }
