8ab3e8290f61847ad09fd4ad1495275cbe520e7c
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-05-18 16:19:03 +0200

    [12.0][IMP] account_skip_bank_reconciliation: allow to specify accounts to be proposed on reconciliation inside bank journal

--- account_skip_bank_reconciliation/models/reconciliation_widget.py
+++ account_skip_bank_reconciliation/models/reconciliation_widget.py
@@ -17,15 +17,10 @@ class AccountReconciliation(models.AbstractModel):
             excluded_ids=excluded_ids, search_str=search_str)
         domain = expression.AND([domain, [
             ("account_id.exclude_bank_reconcile", "!=", True)]])
-        return domain
-
-    @api.model
-    def _domain_move_lines_for_manual_reconciliation(
-            self, account_id, partner_id=False,
-            excluded_ids=None, search_str=False):
-        domain = super()._domain_move_lines_for_manual_reconciliation(
-            account_id, partner_id=partner_id,
-            excluded_ids=excluded_ids, search_str=search_str)
-        domain = expression.AND([domain, [
-            ("account_id.exclude_bank_reconcile", "!=", True)]])
+        # Extract from context allowed accounts defined in Journal, if any
+        journal_id = st_line.journal_id
+        account_reconciliation_ids = journal_id.account_reconciliation_ids
+        if account_reconciliation_ids:
+            domain = expression.AND([domain, [
+                ("account_id", "in", account_reconciliation_ids.ids)]])
         return domain
--- account_skip_bank_reconciliation/readme/DESCRIPTION.rst
+++ account_skip_bank_reconciliation/readme/DESCRIPTION.rst
@@ -1,5 +1,7 @@
 This module allows to exclude from bank statement reconciliation
-all journal items of a specific reconcilable account.
+all journal items of a specific reconcilable account. It also allows
+to specify in the Bank Journal which accounts should be taken into account
+for reconciliation.
 
 Usually, you would want to that in accounts like the
 `Goods Received Not Invoiced`, which are required to be reconcilable
