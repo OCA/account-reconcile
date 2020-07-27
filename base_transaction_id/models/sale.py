0f23586b481bed3e9c64ae58dfc3708f58b854a9
Damien Crier <damien.crier@captivea.com>
2020-07-27 14:39:02 +0200

    [MIG] base_transaction_id: Migration to 13.0

--- base_transaction_id/models/sale.py
+++ base_transaction_id/models/sale.py
@@ -2,22 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     transaction_id = fields.Char(
-        'Transaction ID',
+        "Transaction ID",
         required=False,
         copy=False,
-        help="Transaction id from the financial institute"
+        help="Transaction id from the financial institute",
     )
 
-    @api.multi
     def _prepare_invoice(self):
         """Propagate the transaction_id from the sale order to the invoice."""
         invoice_vals = super()._prepare_invoice()
-        invoice_vals['transaction_id'] = self.transaction_id
+        invoice_vals["transaction_id"] = self.transaction_id
         return invoice_vals
