431efe4af46f5debbd320c1bd18e4c7a8dcfa056
Damien Crier <damien.crier@captivea.com>
2020-07-27 14:39:02 +0200

    [MIG] base_transaction_id: Migration to 13.0

--- base_transaction_id/__manifest__.py
+++ base_transaction_id/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Base transaction ID for financial institutes",
-    "version": "12.0.2.0.0",
+    "version": "13.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Hidden/Dependency",
-    "depends": [
-        "sale",
-    ],
+    "depends": ["sale"],
     "website": "https://github.com/OCA/account-reconcile",
-    "data": [
-        "views/invoice.xml",
-        "views/sale.xml",
-    ],
+    "data": ["views/invoice.xml", "views/sale.xml"],
     "installable": True,
     "license": "AGPL-3",
 }
