ed53232d213c6a67efc0266c7def180d586dd841
João Marques <joao.marques@tecnativa.com>
2021-03-30 11:25:33 +0100

    [FIX] account_reconcile_payment_order: Fix tests
    Create journal instead of searching
    Change hardcoded value for amount of payment statements, after we cleared the taxes of the invoice lines in https://github.com/OCA/bank-payment/pull/798
    
    TT28962

--- account_reconcile_payment_order/__manifest__.py
+++ account_reconcile_payment_order/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Reconcile payment orders",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "Therp BV," "Tecnativa," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-reconcile",
