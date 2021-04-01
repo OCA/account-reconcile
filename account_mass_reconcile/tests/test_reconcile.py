db0e531f7cdf9c242455f6536dc3deba3c146971
Florian da Costa <florian.dacosta@akretion.com>
2021-04-01 15:04:39 +0200

    Manage write off, some refactore to manage better currency, partial reconciliation, and add some tests Also add an advanced reconciliation method based on name. Could be very useful to actually match account move line based on payment_reference field on invoices, as it is passed to account move line on name field

--- account_mass_reconcile/tests/test_reconcile.py
+++ account_mass_reconcile/tests/test_reconcile.py
@@ -1,29 +1,24 @@
 # © 2014-2016 Camptocamp SA (Damien Crier)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import exceptions, fields, tools
-from odoo.modules import get_module_resource
-from odoo.tests import common
+import odoo.tests
+from odoo import exceptions, fields
+
+from odoo.addons.account.tests.common import TestAccountReconciliationCommon
 
 
-class TestReconcile(common.SavepointCase):
+@odoo.tests.tagged("post_install", "-at_install")
+class TestReconcile(TestAccountReconciliationCommon):
     @classmethod
     def setUpClass(cls):
         super(TestReconcile, cls).setUpClass()
-        tools.convert_file(
-            cls.cr,
-            "account",
-            get_module_resource("account", "test", "account_minimal_test.xml"),
-            {},
-            "init",
-            False,
-            "test",
-        )
         cls.rec_history_obj = cls.env["mass.reconcile.history"]
         cls.mass_rec_obj = cls.env["account.mass.reconcile"]
         cls.mass_rec_method_obj = cls.env["account.mass.reconcile.method"]
+
+        cls.sale_journal = cls.company_data["default_journal_sale"]
         cls.mass_rec = cls.mass_rec_obj.create(
-            {"name": "AER2", "account": cls.env.ref("account.a_salary_expense").id}
+            {"name": "Sale Account", "account": cls.sale_journal.default_account_id.id}
         )
         cls.mass_rec_method = cls.mass_rec_method_obj.create(
             {
@@ -33,7 +28,7 @@ class TestReconcile(common.SavepointCase):
             }
         )
         cls.mass_rec_no_history = cls.mass_rec_obj.create(
-            {"name": "AER3", "account": cls.env.ref("account.a_salary_expense").id}
+            {"name": "AER3", "account": cls.sale_journal.default_account_id.id}
         )
         cls.rec_history = cls.rec_history_obj.create(
             {"mass_reconcile_id": cls.mass_rec.id, "date": fields.Datetime.now()}
@@ -57,4 +52,14 @@ class TestReconcile(common.SavepointCase):
 
     def test_prepare_run_transient(self):
         res = self.mass_rec._prepare_run_transient(self.mass_rec_method)
-        self.assertEqual(self.ref("account.a_salary_expense"), res.get("account_id", 0))
+        self.assertEqual(
+            self.sale_journal.default_account_id.id, res.get("account_id", 0)
+        )
+
+    def test_open_full_empty(self):
+        res = self.rec_history._open_move_lines()
+        self.assertEqual([("id", "in", [])], res.get("domain", []))
+
+    def test_open_full_empty_from_method(self):
+        res = self.rec_history.open_reconcile()
+        self.assertEqual([("id", "in", [])], res.get("domain", []))
