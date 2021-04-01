47108f791cf8a453509783b690f7afb3da1f4d04
Florian da Costa <florian.dacosta@akretion.com>
2021-04-01 15:04:39 +0200

    Manage write off, some refactore to manage better currency, partial reconciliation, and add some tests Also add an advanced reconciliation method based on name. Could be very useful to actually match account move line based on payment_reference field on invoices, as it is passed to account move line on name field

--- account_mass_reconcile/models/base_reconciliation.py
+++ account_mass_reconcile/models/base_reconciliation.py
@@ -2,7 +2,6 @@
 # Copyright 2010 Sébastien Beau
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from functools import reduce
 from operator import itemgetter
 
 from odoo import _, fields, models
@@ -46,6 +45,9 @@ class MassReconcileBase(models.AbstractModel):
             "id",
             "debit",
             "credit",
+            "currency_id",
+            "amount_residual",
+            "amount_residual_currency",
             "date",
             "ref",
             "name",
@@ -69,7 +71,8 @@ class MassReconcileBase(models.AbstractModel):
         self.ensure_one()
         where = (
             "WHERE account_move_line.account_id = %s "
-            "AND NOT account_move_line.reconciled"
+            "AND NOT account_move_line.reconciled "
+            "AND parent_state = 'posted'"
         )
         # it would be great to use dict for params
         # but as we use _where_calc in _get_filter
@@ -78,7 +81,7 @@ class MassReconcileBase(models.AbstractModel):
         params = [self.account_id.id]
         if self.partner_ids:
             where += " AND account_move_line.partner_id IN %s"
-            params.append(tuple([l.id for l in self.partner_ids]))
+            params.append(tuple([line.id for line in self.partner_ids]))
         return where, params
 
     def _get_filter(self):
@@ -95,16 +98,29 @@ class MassReconcileBase(models.AbstractModel):
     def _below_writeoff_limit(self, lines, writeoff_limit):
         self.ensure_one()
         precision = self.env["decimal.precision"].precision_get("Account")
-        keys = ("debit", "credit")
-        sums = reduce(
-            lambda line, memo: {
-                key: value + memo[key] for key, value in line.items() if key in keys
-            },
-            lines,
+
+        writeoff_amount = round(
+            sum([line["amount_residual"] for line in lines]), precision
         )
-        debit, credit = sums["debit"], sums["credit"]
-        writeoff_amount = round(debit - credit, precision)
-        return bool(writeoff_limit >= abs(writeoff_amount)), debit, credit
+        writeoff_amount_curr = round(
+            sum([line["amount_residual_currency"] for line in lines]), precision
+        )
+
+        first_currency = lines[0]["currency_id"]
+        if all([line["currency_id"] == first_currency for line in lines]):
+            ref_amount = writeoff_amount_curr
+            same_curr = True
+            # TODO if currency != company currency compute writeoff_limit in currency
+        else:
+            ref_amount = writeoff_amount
+            same_curr = False
+
+        return (
+            bool(writeoff_limit >= abs(ref_amount)),
+            writeoff_amount,
+            writeoff_amount_curr,
+            same_curr,
+        )
 
     def _get_rec_date(self, lines, based_on="end_period_last_credit"):
         self.ensure_one()
@@ -113,10 +129,10 @@ class MassReconcileBase(models.AbstractModel):
             return max(mlines, key=itemgetter("date"))
 
         def credit(mlines):
-            return [l for l in mlines if l["credit"] > 0]
+            return [line for line in mlines if line["credit"] > 0]
 
         def debit(mlines):
-            return [l for l in mlines if l["debit"] > 0]
+            return [line for line in mlines if line["debit"] > 0]
 
         if based_on == "newest":
             return last_date(lines)["date"]
@@ -128,11 +144,51 @@ class MassReconcileBase(models.AbstractModel):
         # when date is None
         return None
 
+    def create_write_off(self, lines, amount, amount_curr, same_curr):
+        self.ensure_one()
+        if amount < 0:
+            account = self.account_profit_id
+        else:
+            account = self.account_lost_id
+        currency = same_curr and lines[0].currency_id or lines[0].company_id.currency_id
+        journal = self.journal_id
+        partners = lines.mapped("partner_id")
+        write_off_vals = {
+            "name": _("Automatic writeoff"),
+            "amount_currency": same_curr and amount_curr or amount,
+            "debit": amount > 0.0 and amount or 0.0,
+            "credit": amount < 0.0 and -amount or 0.0,
+            "partner_id": len(partners) == 1 and partners.id or False,
+            "account_id": account.id,
+            "journal_id": journal.id,
+            "currency_id": currency.id,
+        }
+        counterpart_account = lines.mapped("account_id")
+        counter_part = write_off_vals.copy()
+        counter_part["debit"] = write_off_vals["credit"]
+        counter_part["credit"] = write_off_vals["debit"]
+        counter_part["amount_currency"] = -write_off_vals["amount_currency"]
+        counter_part["account_id"] = (counterpart_account.id,)
+
+        move = self.env["account.move"].create(
+            {
+                "date": lines.env.context.get("date_p"),
+                "journal_id": journal.id,
+                "currency_id": currency.id,
+                "line_ids": [(0, 0, write_off_vals), (0, 0, counter_part)],
+            }
+        )
+        move.action_post()
+        return move.line_ids.filtered(
+            lambda l: l.account_id.id == counterpart_account.id
+        )
+
     def _reconcile_lines(self, lines, allow_partial=False):
         """Try to reconcile given lines
 
         :param list lines: list of dict of move lines, they must at least
-                           contain values for : id, debit, credit
+                           contain values for : id, debit, credit, amount_residual and
+                           amount_residual_currency
         :param boolean allow_partial: if True, partial reconciliation will be
                                       created, otherwise only Full
                                       reconciliation will be created
@@ -143,36 +199,26 @@ class MassReconcileBase(models.AbstractModel):
         """
         self.ensure_one()
         ml_obj = self.env["account.move.line"]
-        below_writeoff, sum_debit, sum_credit = self._below_writeoff_limit(
-            lines, self.write_off
-        )
+        (
+            below_writeoff,
+            amount_writeoff,
+            amount_writeoff_curr,
+            same_curr,
+        ) = self._below_writeoff_limit(lines, self.write_off)
         rec_date = self._get_rec_date(lines, self.date_base_on)
-        line_rs = ml_obj.browse([l["id"] for l in lines]).with_context(
+        line_rs = ml_obj.browse([line["id"] for line in lines]).with_context(
             date_p=rec_date, comment=_("Automatic Write Off")
         )
         if below_writeoff:
-            if sum_credit > sum_debit:
-                writeoff_account = self.account_profit_id
-            else:
-                writeoff_account = self.account_lost_id
-            line_rs.reconcile(
-                writeoff_acc_id=writeoff_account, writeoff_journal_id=self.journal_id
-            )
+            balance = amount_writeoff_curr if same_curr else amount_writeoff
+            if abs(balance) != 0.0:
+                writeoff_line = self.create_write_off(
+                    line_rs, amount_writeoff, amount_writeoff_curr, same_curr
+                )
+                line_rs |= writeoff_line
+            line_rs.reconcile()
             return True, True
         elif allow_partial:
-            # We need to give a writeoff_acc_id
-            # in case we have a multi currency lines
-            # to reconcile.
-            # If amount in currency is equal between
-            # lines to reconcile
-            # it will do a full reconcile instead of a partial reconcile
-            # and make a write-off for exchange
-            if sum_credit > sum_debit:
-                writeoff_account = self.income_exchange_account_id
-            else:
-                writeoff_account = self.expense_exchange_account_id
-            line_rs.reconcile(
-                writeoff_acc_id=writeoff_account, writeoff_journal_id=self.journal_id
-            )
+            line_rs.reconcile()
             return True, False
         return False, False
