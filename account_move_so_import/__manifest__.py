0468b5f00c4a0c8aa1cf303dea273b85bb62e1ae
Florian da Costa <florian.dacosta@akretion.com>
2021-05-11 17:40:18 +0200

    [MIG] account_move_so_import: Migration to 14.0

--- account_move_so_import/__manifest__.py
+++ account_move_so_import/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Journal Entry Sale Order completion",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Finance",
