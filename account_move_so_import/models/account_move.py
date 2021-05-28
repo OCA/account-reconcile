097799b110e4f6509cc50876b480db627002f76f
Florian da Costa <florian.dacosta@akretion.com>
2021-05-28 17:02:51 +0200

    Add completion rule to complete from SO payment ref

--- account_move_so_import/models/account_move.py
+++ account_move_so_import/models/account_move.py
@@ -11,7 +11,13 @@ class AccountMoveCompletionRule(models.Model):
     _inherit = "account.move.completion.rule"
 
     function_to_call = fields.Selection(
-        selection_add=[("get_from_name_and_so", "From line name (based on SO number)")]
+        selection_add=[
+            ("get_from_name_and_so", "From line name (based on SO number)"),
+            (
+                "get_from_name_and_so_payment_ref",
+                "From line name (based on SO payment ref)",
+            ),
+        ]
     )
 
     # Should be private but data are initialized with no update XML
@@ -36,6 +42,35 @@ class AccountMoveCompletionRule(models.Model):
         res = {}
         so_obj = self.env["sale.order"]
         orders = so_obj.search([("name", "=", line.name)])
+        if len(orders) > 1:
+            raise ErrorTooManyPartner(
+                _(
+                    'Line named "%s"  was matched by more '
+                    "than one partner while looking on SO by ref."
+                )
+                % line.name
+            )
+        if len(orders) == 1:
+            res["partner_id"] = orders[0].partner_id.id
+        return res
+
+    # Should be private but data are initialized with no update XML
+    def get_from_name_and_so_payment_ref(self, line):
+        """
+        :param int/long st_line: read of the concerned
+        account.bank.statement.line
+
+        :return:
+            A dict of value that can be passed directly to the write method of
+            the statement line or {}
+           {'partner_id': value,
+            'account_id': value,
+
+            ...}
+        """
+        res = {}
+        so_obj = self.env["sale.order"]
+        orders = so_obj.search([("reference", "=", line.name)])
         if len(orders) > 1:
             raise ErrorTooManyPartner(
                 _(
