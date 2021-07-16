983b52179eda9e7c13451ea290d63f627c792119
AaronHForgeFlow <ahenriquez@eficent.com>
2021-07-16 12:01:56 +0200

    [IMP] account_mass_reconcile_by_purchase_line: black, isort, prettier

--- account_mass_reconcile_by_purchase_line/__manifest__.py
+++ account_mass_reconcile_by_purchase_line/__manifest__.py
@@ -5,17 +5,12 @@
     "name": "Account Mass Reconcile by Purchase Line",
     "summary": "Allows to reconcile based on the PO line",
     "version": "12.0.1.0.0",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-reconcile",
     "category": "Finance",
-    "depends": ["account_mass_reconcile",
-                "account_move_line_purchase_info",
-                ],
+    "depends": ["account_mass_reconcile", "account_move_line_purchase_info",],
     "license": "AGPL-3",
-    "data": [
-        "views/mass_reconcile.xml",
-    ],
+    "data": ["views/mass_reconcile.xml",],
     "installable": True,
     "auto_install": False,
 }
--- account_mass_reconcile_by_purchase_line/models/advanced_reconciliation.py
+++ account_mass_reconcile_by_purchase_line/models/advanced_reconciliation.py
@@ -5,8 +5,8 @@ from odoo import models
 
 
 class MassReconcileAdvancedByPurchaseLine(models.TransientModel):
-    _name = 'mass.reconcile.advanced.by.purchase.line'
-    _inherit = 'mass.reconcile.advanced'
+    _name = "mass.reconcile.advanced.by.purchase.line"
+    _inherit = "mass.reconcile.advanced"
 
     @staticmethod
     def _skip_line(move_line):
@@ -15,15 +15,16 @@ class MassReconcileAdvancedByPurchaseLine(models.TransientModel):
         will be skipped for reconciliation. Can be inherited to
         skip on some conditions. ie: ref or partner_id is empty.
         """
-        return not (move_line.get('product_id') and
-                    move_line.get('purchase_line_id'))
+        return not (move_line.get("product_id") and move_line.get("purchase_line_id"))
 
     @staticmethod
     def _matchers(move_line):
-        return (('product_id', move_line['product_id']),
-                ('purchase_line_id', move_line['purchase_line_id']))
+        return (
+            ("product_id", move_line["product_id"]),
+            ("purchase_line_id", move_line["purchase_line_id"]),
+        )
 
     @staticmethod
     def _opposite_matchers(move_line):
-        yield ('product_id', move_line['product_id'])
-        yield ('purchase_line_id', move_line['purchase_line_id'])
+        yield ("product_id", move_line["product_id"])
+        yield ("purchase_line_id", move_line["purchase_line_id"])
