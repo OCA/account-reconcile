489618e6f77026d112dbd9ca1c9a4ca42b318417
AaronHForgeFlow <ahenriquez@eficent.com>
2021-07-16 12:22:47 +0200

    [IMP] account_mass_reconcile_by_sale_line: black, isort, prettier

--- account_mass_reconcile_by_sale_line/__manifest__.py
+++ account_mass_reconcile_by_sale_line/__manifest__.py
@@ -5,17 +5,12 @@
     "name": "Account Mass Reconcile by Sale Line",
     "summary": "Allows to reconcile based on the SO line",
     "version": "11.0.1.0.0",
-    "author": "ForgeFlow S.L., "
-              "Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-statement-reconcile",
     "category": "Finance",
-    "depends": ["account_mass_reconcile",
-                "account_move_line_sale_info"
-                ],
+    "depends": ["account_mass_reconcile", "account_move_line_sale_info"],
     "license": "AGPL-3",
-    "data": [
-        "views/mass_reconcile.xml",
-    ],
+    "data": ["views/mass_reconcile.xml",],
     "installable": True,
     "auto_install": False,
 }
