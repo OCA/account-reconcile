b7db0dc8cafad1a52508db213e37916cf22d2d0a
Don Kendall <kendall@donkendall.com>
2022-01-22 11:43:18 -0500

    [14.0][MIG] account_move_line_by_sale_line
    [IMP] add security

--- account_mass_reconcile_by_sale_line/__manifest__.py
+++ account_mass_reconcile_by_sale_line/__manifest__.py
@@ -4,13 +4,13 @@
 {
     "name": "Account Mass Reconcile by Sale Line",
     "summary": "Allows to reconcile based on the SO line",
-    "version": "11.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow S.L., " "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/bank-statement-reconcile",
+    "website": "https://github.com/OCA/account-reconcile",
     "category": "Finance",
     "depends": ["account_mass_reconcile", "account_move_line_sale_info"],
     "license": "AGPL-3",
-    "data": ["views/mass_reconcile.xml",],
+    "data": ["security/ir.model.access.csv", "views/mass_reconcile.xml"],
     "installable": True,
     "auto_install": False,
 }
