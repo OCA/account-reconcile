d6b6a368d4d9385b0a6882f82aa25757db1a9ad1
Tosin K <tk@intelligenti.io>
2022-03-07 19:54:09 +0100

    Fixed the reconciliation widget

--- account_reconciliation_widget/models/reconciliation_widget.py
+++ account_reconciliation_widget/models/reconciliation_widget.py
@@ -110,7 +110,7 @@ class AccountReconciliation(models.AbstractModel):
             """
             SELECT "account_move_line".id, COUNT(*) OVER() FROM {from_clause}
             JOIN account_move move ON "account_move_line".move_id = move.id
-            {where_str} AND move.state = "paid"
+            {where_str} 
             ORDER BY ("account_move_line".debit -
                       "account_move_line".credit) = {amount} DESC,
                 "account_move_line".date_maturity ASC,
@@ -118,7 +118,7 @@ class AccountReconciliation(models.AbstractModel):
             {limit_str}
         """.format(
                 from_clause=from_clause,
-                where_str=where_clause and (" WHERE %s" % where_clause) or "",
+                where_str=where_clause and (""" WHERE %s AND move.state = "paid" """ % where_clause) or "",
                 amount=st_line.amount,
                 limit_str=limit and " LIMIT %s" or "",
             )
