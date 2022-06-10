37adf3f40bc52ac2f1f8b498eb229c3073ff4e2d
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-06-10 09:20:11 +0530

    [MIG] account_partner_reconcile: Migration to 15.0

--- account_partner_reconcile/__manifest__.py
+++ account_partner_reconcile/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Partner Reconcile",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-reconcile",
