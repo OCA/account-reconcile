50c4895ee832316f8d24f2267439469784531ea4
Alexis de Lattre <alexis.delattre@akretion.com>
2022-07-01 10:01:32 +0200

    Fix the condition on date >= account_bank_reconciliation_start
    Pre-commit updates

--- account_reconciliation_widget/wizards/res_config_settings.py
+++ account_reconciliation_widget/wizards/res_config_settings.py
@@ -6,8 +6,8 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     account_bank_reconciliation_start = fields.Date(
-        related="company_id.account_bank_reconciliation_start",
-        readonly=False)
+        related="company_id.account_bank_reconciliation_start", readonly=False
+    )
