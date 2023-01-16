9ddff4214b48bd92674b5339ec85124c924a9531
Urvisha-OSI <udesai@opensourceintegrators.com>
2023-01-16 13:32:22 +0530

    [IMP] account_banking_reconciliation: pre-commit stuff

--- account_banking_reconciliation/__manifest__.py
+++ account_banking_reconciliation/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Bank Account Reconciliation",
     "summary": "Check transactions that cleared the bank",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting and Financial Management",
     "author": "NovaPoint Group LLC, "
