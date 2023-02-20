3241887f77635d5446a865b226a194ad270f1692
sonhd91 <sonhd@trobz.com>
2023-02-20 17:59:01 +0700

    [IMP] account_payment_mode_auto_reconcile: black, isort, prettier

--- account_payment_mode_auto_reconcile/models/account_partial_reconcile.py
+++ account_payment_mode_auto_reconcile/models/account_partial_reconcile.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountPartialReconcile(models.Model):
 
-    _inherit = 'account.partial.reconcile'
+    _inherit = "account.partial.reconcile"
 
     payment_mode_auto_reconcile = fields.Boolean()
 
     @api.model
     def create(self, vals):
-        if self.env.context.get('_payment_mode_auto_reconcile'):
-            vals['payment_mode_auto_reconcile'] = True
+        if self.env.context.get("_payment_mode_auto_reconcile"):
+            vals["payment_mode_auto_reconcile"] = True
         return super(AccountPartialReconcile, self).create(vals)
