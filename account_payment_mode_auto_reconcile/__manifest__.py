11c853d1b7e3c5d7820c4690c65e68f00cc6e21a
sonhd91 <sonhd@trobz.com>
2023-02-20 17:59:01 +0700

    [IMP] account_payment_mode_auto_reconcile: black, isort, prettier

--- account_payment_mode_auto_reconcile/__manifest__.py
+++ account_payment_mode_auto_reconcile/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
@@ -6,7 +5,7 @@
     "summary": "Reconcile outstanding credits according to payment mode",
     "version": "10.0.1.0.0",
     "category": "Banking addons",
-    "website": "https://github.com/OCA/bank-payment",
+    "website": "https://github.com/OCA/account-reconcile",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
--- account_payment_mode_auto_reconcile/models/account_payment_mode.py
+++ account_payment_mode_auto_reconcile/models/account_payment_mode.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountPaymentMode(models.Model):
@@ -11,8 +10,8 @@ class AccountPaymentMode(models.Model):
     auto_reconcile_outstanding_credits = fields.Boolean(
         string="Auto reconcile",
         help="Reconcile automatically outstanding credits when an invoice "
-             "using this payment mode is validated, or when this payment mode "
-             "is defined on an open invoice."
+        "using this payment mode is validated, or when this payment mode "
+        "is defined on an open invoice.",
     )
     auto_reconcile_allow_partial = fields.Boolean(
         default=True,
