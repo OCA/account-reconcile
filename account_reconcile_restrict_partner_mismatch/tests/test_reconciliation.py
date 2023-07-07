df8af44fd1eb66b7d855b196458ca5e631b4fff1
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-07 10:58:44 -0300

    [MIG] account_reconcile_restrict_partner_mismatch: Migration to 15.0

--- account_reconcile_restrict_partner_mismatch/tests/test_reconciliation.py
+++ account_reconcile_restrict_partner_mismatch/tests/test_reconciliation.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import UserError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestReconciliation(SavepointCase):
+class TestReconciliation(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
