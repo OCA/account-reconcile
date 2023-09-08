694d0e92e2a9eb96b1963135741db71a8cdfbc08
ferran-73 <ferran@studio73.es>
2023-09-08 12:31:20 +0200

    [16.0][MIG] account_move_reconcile_helper

--- account_move_reconcile_helper/__manifest__.py
+++ account_move_reconcile_helper/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Account Move Reconcile Helper",
     "summary": "Provides tools to facilitate reconciliation",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-reconcile",
-    "depends": ["account_balance_line"],
+    "depends": ["account"],
     "data": ["views/account_move_line.xml"],
     "installable": True,
 }
