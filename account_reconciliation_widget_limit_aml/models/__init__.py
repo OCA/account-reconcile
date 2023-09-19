af13c22f4eff8412baaabcbe72288d433ad0022b
manu <manuel.regidor@sygel.es>
2023-09-19 16:07:07 +0200

    [14.0][ADD]account_reconciliation_widget_limit_aml: Filter all account move lines in reconciliation view

---
+++ account_reconciliation_widget_limit_aml/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2023 Valentin Vinagre <valentin.vinagre@sygel.es>
+# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import res_company
+from . import res_config_settings
+from . import reconciliation_widget
