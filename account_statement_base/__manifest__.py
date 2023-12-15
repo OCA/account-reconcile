166341d919081280407010b2b35cf73034bf5699
Rocío Vega <rov@adhoc.com.ar>
2023-12-15 10:50:29 -0300

    [MIG] account_statement_base: Migration to 17.0

--- account_statement_base/__manifest__.py
+++ account_statement_base/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Bank Statement Base",
-    "version": "16.0.1.3.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "license": "LGPL-3",
     "summary": "Base module for Bank Statements",
