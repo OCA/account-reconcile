7a7b28f155d9354c6957b791f842823220a864d2
Enric Tobella <enric.tobella@dixmit.com>
2024-04-21 23:22:31 +0200

    [IMP] account_reconcile_oca: Add statements information

--- account_reconcile_oca/__manifest__.py
+++ account_reconcile_oca/__manifest__.py
@@ -23,6 +23,7 @@
         "views/account_journal.xml",
         "views/account_move.xml",
         "views/account_account.xml",
+        "views/account_bank_statement.xml",
     ],
     "demo": ["demo/demo.xml"],
     "post_init_hook": "post_init_hook",
