7d80f0554a61c5e69d00d786867db184d96bf0c6
Víctor Martínez <victor.martinez@tecnativa.com>
2025-02-20 09:39:53 +0100

    [FIX] account_reconcile_oca: Replace name_get() to delete the warning log
    WARNING prod py.warnings: /opt/odoo/auto/addons/account_reconcile_oca/models/account_bank_statement_line.py:616: DeprecationWarning: Since 17.0, deprecated method, read display_name instead

--- account_reconcile_oca/models/account_bank_statement_line.py
+++ account_reconcile_oca/models/account_bank_statement_line.py
@@ -613,7 +613,10 @@ class AccountBankStatementLine(models.Model):
                     self.env["res.partner"].browse(line["partner_id"]).display_name,
                 )
             elif self.partner_id:
-                new_line["partner_id"] = self.partner_id.name_get()[0]
+                new_line["partner_id"] = (
+                    self.partner_id.id,
+                    self.partner_id.display_name,
+                )
             new_data.append(new_line)
         return new_data, reconcile_auxiliary_id
 
