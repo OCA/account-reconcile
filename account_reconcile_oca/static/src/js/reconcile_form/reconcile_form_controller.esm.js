8512725ba0533738d7c9d6074d4e2210e6b4fa61
Enric Tobella <enric.tobella@dixmit.com>
2025-05-12 10:08:35 +0200

    [FIX] account_reconcile_oca: Fix reset reconcile button functionality

--- account_reconcile_oca/static/src/js/reconcile_form/reconcile_form_controller.esm.js
+++ account_reconcile_oca/static/src/js/reconcile_form/reconcile_form_controller.esm.js
@@ -21,23 +21,25 @@ export class ReconcileFormController extends FormController {
     async reloadFormController() {
         var is_reconciled = this.model.root.data.is_reconciled;
         await this.model.root.load();
-        if (this.env.parentController) {
+        if (
+            this.env.parentController &&
+            !is_reconciled &&
+            this.model.root.data.is_reconciled
+        ) {
             // We will update the parent controller every time we reload the form.
             await this.env.parentController.model.root.load();
             await this.env.parentController.render(true);
-            if (!is_reconciled && this.model.root.data.is_reconciled) {
-                // This only happens when we press the reconcile button for showing rainbow man
-                const message = await this.orm.call(
-                    "account.journal",
-                    "get_rainbowman_message",
-                    [[this.model.root.data.journal_id[0]]]
-                );
-                if (message) {
-                    this.env.parentController.setRainbowMan(message);
-                }
-                // Refreshing
-                this.env.parentController.selectRecord();
+            // This only happens when we press the reconcile button for showing rainbow man
+            const message = await this.orm.call(
+                "account.journal",
+                "get_rainbowman_message",
+                [[this.model.root.data.journal_id[0]]]
+            );
+            if (message) {
+                this.env.parentController.setRainbowMan(message);
             }
+            // Refreshing
+            this.env.parentController.selectRecord();
         }
     }
 }
