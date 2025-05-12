8a004126464af307a1c9deb914094766246e7b7d
Enric Tobella <enric.tobella@dixmit.com>
2025-05-12 10:30:45 +0200

    [FIX] account_reconcile_oca: Fix reconcile view

--- account_reconcile_oca/static/src/js/reconcile_form/reconcile_form_controller.esm.js
+++ account_reconcile_oca/static/src/js/reconcile_form/reconcile_form_controller.esm.js
@@ -1,6 +1,7 @@
 import {FormController} from "@web/views/form/form_controller";
 import {useService} from "@web/core/utils/hooks";
 import {useViewButtons} from "@web/views/view_button/view_button_hook";
+import {FetchRecordError} from "@web/model/relational_model/errors";
 const {useRef} = owl;
 
 export class ReconcileFormController extends FormController {
@@ -20,23 +21,36 @@ export class ReconcileFormController extends FormController {
     }
     async reloadFormController() {
         var is_reconciled = this.model.root.data.is_reconciled;
-        await this.model.root.load();
+        var forceRefresh = false;
+        const journalId = this.model.root.data.journal_id;
+        try {
+            await this.model.root.load();
+        } catch (e) {
+            // If the record is not found, we need to reload the parent controller
+            if (e instanceof FetchRecordError) {
+                forceRefresh = true;
+            } else {
+                throw e;
+            }
+        }
         if (
             this.env.parentController &&
-            !is_reconciled &&
-            this.model.root.data.is_reconciled
+            (forceRefresh || (!is_reconciled && this.model.root.data.is_reconciled))
         ) {
             // We will update the parent controller every time we reload the form.
             await this.env.parentController.model.root.load();
             await this.env.parentController.render(true);
-            // This only happens when we press the reconcile button for showing rainbow man
-            const message = await this.orm.call(
-                "account.journal",
-                "get_rainbowman_message",
-                [[this.model.root.data.journal_id[0]]]
-            );
-            if (message) {
-                this.env.parentController.setRainbowMan(message);
+            if (journalId) {
+                // This only happens when we press the reconcile button for showing rainbow man
+                // Should not affect if we are in the reconcile view
+                const message = await this.orm.call(
+                    "account.journal",
+                    "get_rainbowman_message",
+                    [[journalId[0]]]
+                );
+                if (message) {
+                    this.env.parentController.setRainbowMan(message);
+                }
             }
             // Refreshing
             this.env.parentController.selectRecord();
