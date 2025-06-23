b131c030cbe9c399a8d18bf3ed677d4c0b9faa40
Areterix <mmalek@areterix.com>
2025-06-23 16:52:06 +0530

    Add payment_reconciliation module for partial settlement in OCA

---
+++ payment_reconciliation/tests/test_payment_reconciliation.py
@@ -0,0 +1,40 @@
+from odoo.exceptions import ValidationError  # Import specific exception
+from odoo.tests.common import TransactionCase
+
+
+class TestPartialSettlement(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.partner = self.env["res.partner"].create({"name": "Test Partner"})
+        self.payment = self.env["account.payment"].create(
+            {
+                "partner_id": self.partner.id,
+                "amount": 500,
+                "payment_type": "inbound",
+                "payment_method_id": self.env.ref(
+                    "account.account_payment_method_manual_in"
+                ).id,
+            }
+        )
+        self.payment.action_post()
+
+    def test_wizard_creation(self):
+        wizard = self.env["partial.settlement.wizard"].create(
+            {
+                "partner_id": self.partner.id,
+                "payment_id": self.payment.id,
+            }
+        )
+        self.assertEqual(wizard.partner_id, self.partner)
+        self.assertEqual(wizard.payment_id, self.payment)
+
+    def test_reconcile_without_amount(self):
+        wizard = self.env["partial.settlement.wizard"].create(
+            {
+                "partner_id": self.partner.id,
+                "payment_id": self.payment.id,
+            }
+        )
+        # Catch the specific exception instead of generic Exception
+        with self.assertRaises(ValidationError):
+            wizard.action_reconcile()
