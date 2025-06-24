4bc90df78098696a557baf583aae6831e6d1e12d
Areterix <mmalek@areterix.com>
2025-06-24 16:44:28 +0530

    UPDATE AND FIXED ALL ODOO WARNING

--- payment_reconciliation/tests/test_payment_reconciliation.py
+++ payment_reconciliation/tests/test_payment_reconciliation.py
@@ -1,5 +1,5 @@
-from odoo.tests.common import TransactionCase
 from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
 
 
 class TestPartialSettlement(TransactionCase):
