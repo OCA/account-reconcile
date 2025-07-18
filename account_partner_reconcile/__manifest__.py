82538d6321eb8a4d940d34ebe3b3179363dfb8e0
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2025-07-18 18:31:03 +0200

    [MIG] Migration to 18.0

--- account_partner_reconcile/__manifest__.py
+++ account_partner_reconcile/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Partner Reconcile",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-reconcile",
