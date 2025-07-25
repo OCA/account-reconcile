52ac39b1b1ad2698ff39a8cf9e7b5c41344f5b13
Antônio Neto <neto@engenere.one>
2025-07-25 20:33:20 -0300

    [IMP] account_reconcile_oca: show open balance

--- account_reconcile_oca/static/src/js/widgets/reconcile_data_widget.esm.js
+++ account_reconcile_oca/static/src/js/widgets/reconcile_data_widget.esm.js
@@ -1,5 +1,7 @@
 /* global CustomEvent */
 import {formatDate, parseDate} from "@web/core/l10n/dates";
+import {getCurrency} from "@web/core/currency";
+import {floatIsZero} from "@web/core/utils/numbers";
 import {formatMonetary} from "@web/views/fields/formatters";
 import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
@@ -28,6 +30,10 @@ export class AccountReconcileDataWidget extends Component {
     }
     getReconcileLines() {
         var data = this.props.record.data[this.props.name].data;
+        const totals = {debit: 0, credit: 0};
+        if (!data || !data.length) {
+            return {lines: [], totals};
+        }
         for (var line in data) {
             data[line].amount_format = formatMonetary(data[line].amount, {
                 currencyId: data[line].currency_id,
@@ -55,8 +61,20 @@ export class AccountReconcileDataWidget extends Component {
             data[line].date_format = formatDate(
                 parseDate(data[line].date, undefined, {isUTC: true})
             );
+            totals.debit += data[line].debit || 0;
+            totals.credit += data[line].credit || 0;
         }
-        return data;
+        totals.balance = totals.debit - totals.credit;
+        const [firstLine = {}] = Object.values(data);
+        const currency = getCurrency(firstLine.currency_id);
+        const decimals = currency.digits[1];
+        const hasOpenBalance = !floatIsZero(totals.balance, decimals);
+        const absoluteBalance = Math.abs(totals.balance);
+        const openDebitFmt =
+            totals.balance < 0 ? formatMonetary(absoluteBalance, {currency}) : null;
+        const openCreditFmt =
+            totals.balance > 0 ? formatMonetary(absoluteBalance, {currency}) : null;
+        return {lines: data, hasOpenBalance, openDebitFmt, openCreditFmt};
     }
     onTrashLine(ev, line) {
         ev.stopPropagation();
